068a337b7cc4b6b158e1d2759b35926bd8833b7a
Hessam-Hosseininan <hessam2004h@gmail.com>
2025-01-13 17:30:36 +0330

    save commit

--- src/group7/urls.py
+++ src/group7/urls.py
@@ -6,20 +6,23 @@ from .views import *
 
 app_name = 'group7'
 urlpatterns = [
-    # path('', views.home, name='home'),
-    path('', views.home, name='group7'),
-    path('about', views.about, name='about'),
-    # path('search/', exact_search_words, name='exact_search_words'),
-    # path('index/', views.index, name='index'),
-    # path('about/', views.about, name='about'),
-    # path('login/', views.login_user, name='login'),
-    # path('logout/', views.logout_user, name='logout'),
-    # path('register/', views.register_user, name='register'),
-    # path('profile/', views.user_profile, name='user_profile'),
-    #
-    # path('searchpage/', views.searchPage, name='searchPage'),
-    #
-    #
-    # path('wordcart/<str:favoriteword>/', views.wordcart, name='wordcart'),
+                  # path('', views.home, name='home'),
+                  path('', views.home, name='group7'),
 
-]
+                  path('about/', views.about, name='about'),
+
+                  path('main/', views.index, name='index'),
+                  path('search/', exact_search_words, name='exact_search_words'),
+
+                  # path('about/', views.about, name='about'),
+                  # path('login/', views.login_user, name='login'),
+                  # path('logout/', views.logout_user, name='logout'),
+                  # path('register/', views.register_user, name='register'),
+                  # path('profile/', views.user_profile, name='user_profile'),
+                  #
+                  # path('searchpage/', views.searchPage, name='searchPage'),
+                  #
+                  #
+                  # path('wordcart/<str:favoriteword>/', views.wordcart, name='wordcart'),
+
+              ]
--- src/group7/views.py
+++ src/group7/views.py
@@ -1,4 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+import logging
+from hazm import *
+from meilisearch import Client
+
+from group7.apps import logger
+
 
 # Create your views here.
 
@@ -10,6 +17,79 @@ def home(request):
 def about(request):
     return render (request , 'about.html' )
 
+def index(request):
+    return render (request , 'index.html')
 
 
+
+
+def get_meilisearch_client():
+    return Client('http://127.0.0.1:7700')
+
+
+def _normalize_query(query: str) -> str:
+    suffixes = [' ها', 'ان', ' ها', 'ها','ی', 'ي', ]
+    for suffix in suffixes:
+        if query.endswith(suffix):
+            query = query[:-len(suffix)]
+            break
+
+    query = lemmatizer.lemmatize(query)
+    query2 = query.split("#")
+    query = query2[0]
+
+    return query.strip()
+
+
+def exact_search_words(request):
+    query = request.GET.get('q', '').strip()
+    if not query:
+        return JsonResponse({"error": "Query parameter 'q' is required."}, status=400)
+
+    try:
+        client = get_meilisearch_client()
+        index = client.index('words')
+
+        filter_query = f'word = "{query}"'
+
+        results = index.search("", {
+            "limit": 1,
+            "filter": filter_query,
+        })
+
+        if not results.get('hits', []):
+
+            return search_words(request)
+
+        return JsonResponse(results.get('hits', []), safe=False)
+
+    except Exception as e:
+        logger.exception("Unexpected error during search")
+        return JsonResponse({"error": "An unexpected error occurred during the search."}, status=500)
+
+
+def search_words(request):
+    query = request.GET.get('q', '').strip()
+    if not query:
+        return JsonResponse({"error": "Query parameter 'q' is required."}, status=400)
+
+
+    query = _normalize_query(query)
+
+    try:
+
+        client = get_meilisearch_client()
+        index = client.index('words')
+
+
+        results = index.search(query, {
+            "limit": 1,
+        })
+
+
+        return JsonResponse(results.get('hits', []), safe=False)
+
+    except Exception as e:
+        logger.exception("Unexpected error during search")
+        return JsonResponse({"error": "An unexpected error occurred during the search."}, status=500)
 
