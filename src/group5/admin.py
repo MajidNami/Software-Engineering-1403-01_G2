9753b6b567db85b91f9f2ae910ba28205eb059e0
mehrshad-kh <mjii@protonmail.com>
2025-01-15 14:32:29 +0330

    fix: move all files from group5/ to group4/

--- src/group5/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import G25Dataset
-
-
-@admin.register(G25Dataset)
-class G25DatasetAdmin(admin.ModelAdmin):
-    list_display = ('id', 'word')
-    search_fields = ('word',)
--- src/group5/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class G25Dataset(models.Model):
-    id = models.AutoField(primary_key=True)
-    word = models.CharField(max_length=255)
-
-    class Meta:
-        db_table = 'G2_5_dataset'
